4023b32a06349bea8756f0ef357c61ee36003c2a
Siddhanth <siddhanthgreat@gmail.com>
2026-02-14 21:16:49 +0530

    updated vite.config.ts to set base path for GitHub Pages deployment, and updated the title in App.tsx to "us being us."

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,9 @@ export default defineConfig(({ command }) => {
   // If you are deploying to a GitHub Project Page (e.g. https://username.github.io/repo-name/),
   // you MUST uncomment the lines below and replace 'YOUR_REPO_NAME' with your actual repository name.
   //
-  // if (command !== 'serve') {
-  //   config.base = '/YOUR_REPO_NAME/'
-  // }
+  if (command !== 'serve') {
+    config.base = '/hii2/'
+   }
 
   return config
 })
